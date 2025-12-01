6e02e15129add7e153fc9e5e71439bc86af20256
hexselarchieles <hexselavmm@gmail.com>
2025-12-01 12:38:06 +0700

    Commit No. 2 – Building app with coroutines

--- app/src/main/java/com/example/test_lab_week_12/MovieRepository.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieRepository.kt
@@ -1,7 +1,26 @@
 package com.example.test_lab_week_12
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.test_lab_week_12.api.MovieService
+import com.example.test_lab_week_12.model.Movie
 
 class MovieRepository(private val movieService: MovieService) {
 
+    private val apiKey = "05f4858c59e07e3b764031e70230ea05"
+
+    private val movieLiveData = MutableLiveData<List<Movie>>()
+    val movies: LiveData<List<Movie>> get() = movieLiveData
+
+    private val errorLiveData = MutableLiveData<String>()
+    val error: LiveData<String> get() = errorLiveData
+
+    suspend fun fetchMovies() {
+        try {
+            val popularMovies = movieService.getPopularMovies(apiKey)
+            movieLiveData.postValue(popularMovies.results)
+        } catch (exception: Exception) {
+            errorLiveData.postValue("An error occurred: ${exception.message}")
+        }
+    }
 }
